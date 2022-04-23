f45658761b139b76ecd717b2187d500069dc0216
Yolan Anjani Usen <54465467+yolan26@users.noreply.github.com>
2022-04-23 13:10:25 +0700

    Add files via upload

---
+++ Apk_CoffeeShop/src/apk_coffeeshop/koneksi.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package apk_coffeeshop;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ */
+public class koneksi {
+   private String url = "jdbc:mysql://localhost/toko_kopi";
+   private String username_xampp = "root";
+   private String password = "";
+   private Connection con;
+   
+   public void connect(){
+       try {
+           con = DriverManager.getConnection(url, username_xampp, password);
+           System.out.println("Koneksi Berhasil !");
+       } catch (Exception e) {
+           JOptionPane.showMessageDialog(null, e.getMessage());
+       }
+   }
+   
+   public Connection getCon() {
+       return con;
+   }
+}
